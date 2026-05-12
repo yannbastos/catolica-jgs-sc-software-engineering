44f8432a51613367424d16439d0689290b3f0162
yannfbastos <yannfelipebastos52559@gmail.com>
2026-05-12 12:59:48 -0300

    feat: adiciona programa que conta elementos > 10 em matriz 4x4

--- 2026-1_Primeiro_Semestre/T1ESOFT01NA_FUNDAMENTOS_DE_PROGRAMACAO_202611/AULA_10/Atividade-10.c
+++ 2026-1_Primeiro_Semestre/T1ESOFT01NA_FUNDAMENTOS_DE_PROGRAMACAO_202611/AULA_10/Atividade-10.c
@@ -1,2 +1,35 @@
 // 10. Construa um programa que leia uma matriz 4x4 e conte quantos elementos são maiores que 10. Ao final, exiba a quantidade encontrada. 
 // R.:
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int matriz[4][4];
+    int i, j, contador = 0;
+    
+    printf("Digite os elementos da matriz 4x4:\n");
+    
+    // Lendo os elementos da matriz
+    for(i = 0; i < 4; i++) {
+        for(j = 0; j < 4; j++) {
+            printf("Elemento [%i][%i]: ", i, j);
+            scanf("%i", &matriz[i][j]);
+            fflush(stdin); // Limpa o buffer do teclado
+        }
+    }
+    
+    // Contando elementos maiores que 10
+    for(i = 0; i < 4; i++) {
+        for(j = 0; j < 4; j++) {
+            if(matriz[i][j] > 10) {
+                contador++;
+            }
+        }
+    }
+    
+    // Exibindo o resultado
+    printf("\nQuantidade de elementos maiores que 10: %i\n", contador);
+    
+    return 0;
+}
